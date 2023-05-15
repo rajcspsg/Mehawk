34ee4f1a45b12d78aafafacd89634901d546a502
b4mbus <danielzaradny@gmail.com>
2023-05-15 05:05:33 +0200

    meta: port from flake-utils to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,42 +1,46 @@
 {
+  description = "Fill this later, please.";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in {
-      formatter = pkgs.alejandra;
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
 
-      devShells.default = with pkgs; mkShell.override {
-          stdenv = lowPrio llvmPackages_16.stdenv;
-        } {
-          buildInputs = [
-            clang-tools_16
-            cmake
-            pkg-config
+        devShells.default = with pkgs;
+          mkShell.override {
+            stdenv = lowPrio llvmPackages_16.stdenv;
+          } {
+            buildInputs = [
+              clang-tools_16
+              cmake
+              pkg-config
 
-            just
-            ninja
-            meson
+              just
+              ninja
+              meson
 
-            openssl
-            python311
-            gtkmm4
-          ];
+              openssl
+              python311
+              gtkmm4
+            ];
 
-          env = {
-            CLANGD_PATH = "${pkgs.clang-tools_16}/bin/clangd";
+            env = {
+              CLANGD_PATH = "${pkgs.clang-tools_16}/bin/clangd";
+            };
           };
-        };
-
-    });
+      };
+    };
 }
